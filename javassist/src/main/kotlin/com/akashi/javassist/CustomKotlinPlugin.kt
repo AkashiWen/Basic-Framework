8729cf236933b10a9f99b52ce693d3e0da11f51c
Akashiwen <343611864@qq.com>
2022-05-05 20:33:06 +0800

    feat: register transform

--- javassist/src/main/kotlin/com/akashi/javassist/CustomKotlinPlugin.kt
+++ javassist/src/main/kotlin/com/akashi/javassist/CustomKotlinPlugin.kt
@@ -1,10 +1,14 @@
 package com.akashi.javassist
 
+import com.akashi.javassist.transforms.CustomTransform
+import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class CustomKotlinPlugin : Plugin<Project> {
-    override fun apply(p0: Project) {
+    override fun apply(project: Project) {
         println("-----Akashi Kotlin Plugin------")
+        project.extensions.getByType(AppExtension::class.java)
+            .registerTransform(CustomTransform(project))
     }
 }
